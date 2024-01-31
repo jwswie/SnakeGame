f08694efeeafa496a404b597ee40bd7eaf2ccf9e
Desik <Desik@DESKTOP-995B0FP>
2024-01-31 19:42:21 +0100

    Basic Movement(without AWSD)

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,9 +22,36 @@ namespace SnakeGame
     
     public partial class MainWindow : Window
     {
+        Snake Snake { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Snake = new Snake(4);
+            Task.Run(() => Game());
+        }
+        private async Task Game() 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Dispatcher.InvokeAsync(() =>
+                {
+                    DrawSnake();
+                });
+                await Task.Delay(1000);
+                Snake.MoveSnake();
+            }
+        }
+        private void DrawSnake() 
+        {
+            MainGrid.Children.Clear();
+            foreach (var body in Snake.SnakeBody)
+            {
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Background = new SolidColorBrush(Colors.Black);
+                Grid.SetColumn(stackPanel, body.x);
+                Grid.SetRow(stackPanel, body.y);
+                MainGrid.Children.Add(stackPanel);
+            }
         }
     }
 }
--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -8,5 +8,60 @@ namespace SnakeGame
 {
     public class Snake
     {
+        public class Body
+        {
+            public int x;
+            public int y;
+        }
+        int dir;
+        public List<Body> SnakeBody { get; set; }
+        public Snake(int BodyLength) 
+        {
+            SnakeBody = new List<Body>();
+            dir = 2;
+            for (int i = 0; i < BodyLength; i++)
+            {
+                SnakeBody.Add(new Body() { x = 4-i, y = 4 });
+            }
+        }
+        public void MoveSnake() 
+        {
+
+            switch (dir)
+            {
+                case 0:
+                    for (int i = SnakeBody.Count - 1; i > 0; i--)
+                    {
+                        SnakeBody[i].y = SnakeBody[i-1].y;
+                        SnakeBody[i].x = SnakeBody[i-1].x;
+                    }
+                    SnakeBody[0].y -=1;
+                    break;
+                case 1:
+                    for (int i = SnakeBody.Count - 1; i > 0; i--)
+                    {
+                        SnakeBody[i].y = SnakeBody[i-1].y;
+                        SnakeBody[i].x = SnakeBody[i-1].x;
+                    }
+                    SnakeBody[0].x -=1;
+                    break;
+                case 2:
+                    for (int i = SnakeBody.Count - 1; i > 0; i--)
+                    {
+                        SnakeBody[i].y = SnakeBody[i-1].y;
+                        SnakeBody[i].x = SnakeBody[i-1].x;
+                    }
+                    SnakeBody[0].x +=1;
+                    break;
+                case 3:
+                    for (int i = SnakeBody.Count - 1; i > 0; i--)
+                    {
+                        SnakeBody[i].y = SnakeBody[i-1].y;
+                        SnakeBody[i].x = SnakeBody[i-1].x;
+                    }
+                    SnakeBody[0].y +=1;
+                    break;
+            }
+        }
     }
 }
