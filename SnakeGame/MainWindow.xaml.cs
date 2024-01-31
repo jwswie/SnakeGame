5100d9090e05c8918c3d4f86f5aa3c3d0c531f72
Desik <Desik@DESKTOP-995B0FP>
2024-01-31 18:30:11 +0100

    Send

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace SnakeGame
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
